1149cb069cc9b4ca62463f59e449c17cec54d874
Yann Morlon <yann25m@gmail.com>
2023-04-27 16:47:58 +0200

    Version 3.0.2
    Fix curl SSL verify

--- numerique.php
+++ numerique.php
@@ -17,7 +17,8 @@ curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET"
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_SSL_VERIFYPEER => false
 ));
 
 $response = curl_exec($curl);
