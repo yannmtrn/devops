429d5bf87b7ed7f9e856699c14e3af2ebc130adc
Yann Morlon <yann25m@gmail.com>
2023-04-28 09:23:26 +0200

    Version 3.0.3
    Fix curl SSL verifyhost

--- synthese.php
+++ synthese.php
@@ -18,7 +18,8 @@ curl_setopt_array($curl, array(
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_SSL_VERIFYPEER => false
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => 0
 ));
 
 $response = curl_exec($curl);
